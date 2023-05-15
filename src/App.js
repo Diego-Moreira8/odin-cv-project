2519c2ca4ed031a3ef558c99c576480d0d3983be
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2023-05-15 09:13:00 -0300

    Added footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/header";
 import Profile from "./components/profile";
 import Education from "./components/education";
 import Experience from "./components/experience";
+import MyFooter from "./components/footer";
 
 class App extends Component {
   constructor() {
@@ -20,6 +21,7 @@ class App extends Component {
           <Education />
           <Experience />
         </div>
+        <MyFooter />
       </div>
     );
   }
